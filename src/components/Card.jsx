4e4a8e4a9f1085961e9073b758eaf5c3bd7a21f0
stylish-pawmy <ambesi2026@gmail.com>
2023-09-04 09:10:15 +0100

    Add photo component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "../App.css";
+import { Photo } from "./Photo.jsx";
 
 export function Card() {
     return (
         <>
-            
+            <Photo />
         </>
     );
 }
---
+++ src/components/Photo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+import PersonPhoto from "../assets/photo.png";
+
+export function Photo()
+{
+    return (
+        <>
+            <img src= { PersonPhoto } />
+        </> 
+    )
+}
+
+export default Photo
