96eda4a0def4368fd7c86f0638128473afd433e5
stylish-pawmy <ambesi2026@gmail.com>
2023-09-04 09:20:06 +0100

    Add main section with headlines

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../App.css";
 import { Photo } from "./Photo.jsx";
+import { Main } from "./Main.jsx";
 
 export function Card() {
     return (
         <>
             <Photo />
+            <Main />
         </>
     );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+
+export function Main() {
+    return (
+        <div className="main-content">
+            <h1 className="full-name">Laura Smith</h1>
+            <h4 className="occupation">Frontend Developer</h4>
+            <p className="website-link">laurasmith.website</p>
+        </div>
+    );
+}
+
+export default Main
