fe2f86bc10e898b1092238228784d2febaee206a
stylish-pawmy <ambesi2026@gmail.com>
2023-09-04 09:05:05 +0100

    Add card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import './App.css'
+import './App.css';
+import { Card } from "./components/Card.jsx";
 
 function App() {
   return (
     <>
+      <Card />
     </>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+export function Card() {
+    return (
+        <>
+            
+        </>
+    );
+}
+
+export default Card
