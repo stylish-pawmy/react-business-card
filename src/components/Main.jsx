7fc8f33f1d64729f833f757e02e2273529c2c41b
stylish-pawmy <ambesi2026@gmail.com>
2023-09-06 04:43:56 +0100

    Add body text

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,10 +9,22 @@ export function Main() {
             <h1 className="full-name">Laura Smith</h1>
             <h4 className="occupation">Frontend Developer</h4>
             <p className="website-link">laurasmith.website</p>
+
             <div className="buttons-container">
                 <EmailButton />
                 <LinkedinButton />
             </div>
+
+            <h5 className="body-header">About</h5>
+            <p className="body-text">I am a frontend developer with a particular interest
+                 in making things simple and automating daily tasks.
+                  I try to keep up with security and best practices,
+                 and am always looking for new things to learn.</p>
+
+            <h5 className="body-header">Interests</h5>
+            <p className="body-text">Food expert. Music scholar. Reader. Internet fanatic.
+                 Bacon buff. Entrepreneur. Travel geek. Pop culture
+                 ninja. Coffee fanatic.</p>
         </div>
     );
 }
